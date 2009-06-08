4a59d55b8a4a33746c6cbd4804accc391bbf72dc
c.borgolte@hudora.de <c.borgolte@hudora.de@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2009-06-08 08:15:13 +0000

    Quickfix for delivery times.
    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/projects/benEDIct/trunk@5787 2a7d6d32-97f8-0310-8fd3-c67d3a265405

--- cctop/orders.py
+++ cctop/orders.py
@@ -197,7 +197,7 @@ transaktionstermine = [ # kopiert von satzart 515
          default=' ' * 6, doc="DTM-2380"),
     dict(startpos=86, endpos=91, length=6, name='lieferwoche_zugesagt', fieldclass=FixedField,
          default=' ' * 6, doc="DTM-2380"),
-    dict(startpos=92, endpos=95, length=4, name='lieferuhrzeit', fieldclass=FixedField,
+    dict(startpos=92, endpos=95, length=4, name='lieferuhrzeit',# fieldclass=FixedField,
          default=' ' * 4, doc="DTM-2380"),
     dict(startpos=96, endpos=99, length=4, name='lieferuhrzeit_earliest', fieldclass=FixedField,
          default=' ' * 4, doc="DTM-2380"),
