32e1c28d8efde5721c54ecf6030134632b2f4caf
Maximillian Dornseif <md@hudora.de>
2010-09-07 07:58:20 +0200

    new directory structure, first cleanup attempts

--- edilib/softm/__init__.py
+++ edilib/softm/__init__.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-__init__.py
+edilib.softm/__init__.py - parsing of SoftM "EDI Format"
 
 Created by Maximillian Dornseif on 2010-09-07.
 Copyright (c) 2010 HUDORA. All rights reserved.
 """
+
+from edilib.softm.structure import *
+from edilib.softm.content import *
+
